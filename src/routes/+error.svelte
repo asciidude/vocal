98cf51a69fca0f80f73e44ec4d993c84acb83bc9
Brando <asciidude0@gmail.com>
2025-03-07 23:25:58 -0600

    Update error and index page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -7,6 +7,6 @@
 <main>
     <div class="h-screen flex flex-col items-center justify-center text-center text-white w-96 mx-auto">
         <img src="https://http.cat/{page.status}" alt="{page.status} ({page.error?.message})" class="max-[384px]:h-40">
-        <p class="mt-5 text-sm italic text-vocal underline"><a href="/">Back home</a></p>
+        <p class="mt-5 text-sm italic text-vocal_light underline"><a href="/">Back home</a></p>
     </div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,24 +9,22 @@
 <main class="background">
     <div class="h-screen flex flex-col items-center justify-center text-center text-white w-96 mx-auto">
         <img src="images/vocal-icon-circle.png" alt="Vocal Icon" class="w-40">
-        <p class="mt-5 text-xl italic">Welcome to <span class="text-vocal font-bold">Vocal</span></p>
+        <p class="mt-5 text-xl italic">Welcome to <span class="text-vocal_light font-bold">Vocal</span></p>
         
         <Button class="bg-black/55 hover:bg-gray-800/55 w-40 mt-5" href='/api/auth/discord'>
             <span>Login with Discord</span>
             <span><LogIn class="ml-2 inline-block size-5" color="#fff" /></span>
         </Button>
-
-        <!-- TODO: Come up with a system better than this. User roles might come in handy -->
          
         <Alert.Root class="mt-5 bg-black/55 border-none text-white max-[392px]:w-40">
             <TriangleAlert class="h-4 w-4" color="#fff" />
-            <Alert.Title>You are required to have the beta tester role.</Alert.Title>
+            <Alert.Title>Account approval required</Alert.Title>
             <Alert.Description class="italic">
-                Don't have a role? Ask a friend who does and see if they have any spare keys!
+                As Vocal is still in beta, your account must be approved by a superadmin. Ask through Discord!
             </Alert.Description>
         </Alert.Root>
 
-        <p class="mt-5 italic underline text-vocal"><a href="#">Join our Discord</a></p>
+        <p class="mt-5 italic underline text-vocal"><a href="https://discord.gg/4Rwr2pu2bW">Join our Discord</a></p>
 
         <!-- TODO: Add footer with privacy policy and terms of service -->
     </div>
