3444c747988df6f2e744029df6dbad7c4c59f516
Brando <asciidude0@gmail.com>
2025-11-18 08:51:02 -0600

    fix/improve seeding

--- src/routes/api/seed/+server.ts
+++ src/routes/api/seed/+server.ts
@@ -8,9 +8,10 @@ import type { AttachmentType } from "$lib/types/Attachment.type";
 import { FollowModel } from "$lib/models/Follow.model";
 import { LikeModel } from "$lib/models/Like.model";
 import type { AuthProviderType } from "src/lib/types/AuthProvider.type";
+import { computeDocumentVector, tfidf } from "src/lib/utils/TF-IDF.util";
 
 export const GET: RequestHandler = async() => {
-    if(NODE_ENV === 'production') throw error(403, 'Unable to seed database in production envrionment');
+    if(NODE_ENV === 'production') throw error(403, 'Unable to seed database in production environment');
 
     await UserModel.deleteMany();
     await UserModel.insertMany([{
@@ -26,7 +27,11 @@ export const GET: RequestHandler = async() => {
         displayName: 'Gseppo',
 
         bio: 'Hi, my name is Gseppo! How are you!?',
-        roles: [UserRoles.Beta, UserRoles.Tester]
+        roles: [UserRoles.Beta, UserRoles.Tester],
+
+        userInterestVectors: {},
+        followedHashtags: ['programming', 'tech', 'javascript', 'webdev'],
+        excludedKeywords: []
     },
     {
         authProviders: [{
@@ -41,43 +46,146 @@ export const GET: RequestHandler = async() => {
         displayName: 'gone.',
 
         bio: 'Hello, I am gone. What\'s good?',
-        roles: [UserRoles.Beta]
+        roles: [UserRoles.Beta],
+        
+        userInterestVectors: {},
+        followedHashtags: ['gaming', 'music', 'art', 'creative'],
+        excludedKeywords: []
     }]);
 
     const gseppo = await UserModel.findOne({ username: 'gseppo' });
     const gone = await UserModel.findOne({ username: '2deepyt' });
 
+    const gseppoPosts = [
+        'Just made a revolutionary social media app. #programming #webdev',
+        'I freaking love MongoDB!!!!!!! #tech #database',
+        'React < Svelte #webdev',
+        'I don\'t know what to post',
+        'Hello!!'
+    ];
+
+    const gonePosts = [
+        'Wow... what a game. #gaming #eldenring',
+        'FL Studio is the goat #music #production',
+        'Working on some designs for my album cover. #art #creative #music',
+        'Live on Twitch w/ my friends! #gaming #friends #twitch',
+        'Exploring new music genres lately... 8)'
+    ];
+
+    [...gseppoPosts, ...gonePosts].forEach(content => tfidf.addDocument(content));
+
     await PostModel.deleteMany();
-    const post = await PostModel.insertOne({
-        author: gseppo!._id,
-        content: 'Hello world!',
+    
+    const gseppoPostDocs = await Promise.all(
+        gseppoPosts.map(async (content) => {
+            return await PostModel.create({
+                author: gseppo!._id,
+                content,
+                attachments: [],
+                postVectors: computeDocumentVector(content)
+            });
+        })
+    );
+
+    const gonePostDocs = await Promise.all(
+        gonePosts.map(async (content) => {
+            return await PostModel.create({
+                author: gone!._id,
+                content,
+                attachments: [],
+                postVectors: computeDocumentVector(content)
+            });
+        })
+    );
+
+    await ReplyModel.deleteMany();
+    await ReplyModel.create({
+        parent_post: gseppoPostDocs[0]._id,
+        author: gone!._id,
+        content: 'Dude I freaking love Svelte',
         attachments: []
     });
 
-    await ReplyModel.deleteMany();
-    await ReplyModel.insertOne({
-        parent_post: post._id,
-        author: gone!._id,
-        content: 'Hello ?_?',
-        attachments: [{
-            url: 'https://media.tenor.com/6kYNH_1ry08AAAAi/hello.gif',
-            type: 'image'
-        } as AttachmentType]
+    await ReplyModel.create({
+        parent_post: gonePostDocs[0]._id,
+        author: gseppo!._id,
+        content: 'RIGHT!?',
+        attachments: []
     });
     
     await FollowModel.deleteMany();
-    await FollowModel.insertOne({
+    await FollowModel.create({
         followerId: gseppo!._id,
         followingId: gone!._id
     });
 
+    await FollowModel.create({
+        followerId: gone!._id,
+        followingId: gseppo!._id
+    });
+
     await LikeModel.deleteMany();
-    await LikeModel.insertOne({
-        parent_post: post._id,
+    await LikeModel.create({
+        parent_post: gseppoPostDocs[0]._id,
         author: gone!._id
     });
 
+    await LikeModel.create({
+        parent_post: gonePostDocs[1]._id,
+        author: gseppo!._id
+    });
+
+    await LikeModel.create({
+        parent_post: gseppoPostDocs[2]._id,
+        author: gone!._id
+    });
+
+    await LikeModel.create({
+        parent_post: gonePostDocs[0]._id,
+        author: gseppo!._id
+    });
+
+    if (gseppo) {
+        const gseppoVector = { ...(gseppo.userInterestVectors || {}) };
+        
+        const likedPosts = [gonePostDocs[1], gonePostDocs[0]];
+        likedPosts.forEach(post => {
+            const postVector = post.postVectors || {};
+            for (const [token, weight] of Object.entries(postVector)) {
+                gseppoVector[token] = (gseppoVector[token] || 0) + weight;
+            }
+        });
+        
+        const mag = Math.sqrt(Object.values(gseppoVector).reduce((s, v) => s + v*v, 0)) || 1;
+        for (const k in gseppoVector) {
+            gseppoVector[k] = gseppoVector[k] / mag;
+        }
+        
+        gseppo.userInterestVectors = gseppoVector;
+        await gseppo.save();
+    }
+
+    if (gone) {
+        const goneVector = { ...(gone.userInterestVectors || {}) };
+        
+        const likedPosts = [gseppoPostDocs[0], gseppoPostDocs[2]];
+        likedPosts.forEach(post => {
+            const postVector = post.postVectors || {};
+            for (const [token, weight] of Object.entries(postVector)) {
+                goneVector[token] = (goneVector[token] || 0) + weight;
+            }
+        });
+        
+        const mag = Math.sqrt(Object.values(goneVector).reduce((s, v) => s + v*v, 0)) || 1;
+        for (const k in goneVector) {
+            goneVector[k] = goneVector[k] / mag;
+        }
+        
+        gone.userInterestVectors = goneVector;
+        await gone.save();
+    }
+
     return json({
-        message: 'The database has been seeded.'
+        message: `Database seeded with ${gseppoPosts.length + gonePosts.length} posts and user interest vectors!`
     });
 }
