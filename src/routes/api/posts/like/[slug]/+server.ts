f0c627c2211588450cf04af13b710210ec354e7b
Brando <asciidude0@gmail.com>
2025-11-18 07:43:32 -0600

    improve like/unlike vectorization

--- src/routes/api/posts/like/[slug]/+server.ts
+++ src/routes/api/posts/like/[slug]/+server.ts
@@ -1,9 +1,3 @@
-/**
- * Like/unlike should be handled here,
- * extra information should be added to the user object
- * regarding the post such as categories...later.
- */
-
 import { error, json, type RequestHandler } from "@sveltejs/kit";
 import { LikeModel } from '$lib/models/Like.model';
 import { PostModel } from "src/lib/models/Post.model";
@@ -26,19 +20,24 @@ export const POST: RequestHandler = async({ params, request, locals }) => {
         throw error(401, 'Unauthorized');
     }
 
-    const updatedVector = { ...(userDoc.userInterestVectors || {}) };
-
-    for (const [token, weight] of Object.entries(post.postVectors || {})) {
-        updatedVector[token] = (updatedVector[token] || 0) + weight;
-    }
-
-    userDoc.userInterestVectors = updatedVector;
-    await userDoc.save();
-
     try {
         const like = await LikeModel.exists({ parent_post: postId, author: user._id });
 
         if(!like) {
+            const updatedVector = { ...(userDoc.userInterestVectors || {}) };
+
+            for (const [token, weight] of Object.entries(post.postVectors || {})) {
+                updatedVector[token] = (updatedVector[token] || 0) + weight;
+            }
+
+            const mag = Math.sqrt(Object.values(updatedVector).reduce((s, v) => s + v*v, 0)) || 1;
+            for (const k in updatedVector) {
+                updatedVector[k] = updatedVector[k] / mag;
+            }
+
+            userDoc.userInterestVectors = updatedVector;
+            await userDoc.save();
+
             await LikeModel.create({
                 parent_post: postId,
                 author: user._id
@@ -50,6 +49,25 @@ export const POST: RequestHandler = async({ params, request, locals }) => {
                 message: 'Success'
             });
         } else {
+            const updatedVector = { ...(userDoc.userInterestVectors || {}) };
+
+            for (const [token, weight] of Object.entries(post.postVectors || {})) {
+                if (updatedVector[token] !== undefined) {
+                    updatedVector[token] = Math.max(0, updatedVector[token] - weight);
+                    if (updatedVector[token] < 0.001) {
+                        delete updatedVector[token];
+                    }
+                }
+            }
+
+            const mag = Math.sqrt(Object.values(updatedVector).reduce((s, v) => s + v*v, 0)) || 1;
+            for (const k in updatedVector) {
+                updatedVector[k] = updatedVector[k] / mag;
+            }
+
+            userDoc.userInterestVectors = updatedVector;
+            await userDoc.save();
+
             await LikeModel.deleteOne({
                 parent_post: postId,
                 author: user._id
