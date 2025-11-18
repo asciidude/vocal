158da0b336ce179d3de567323ec29c1b37ee0f65
Brando <asciidude0@gmail.com>
2025-11-18 06:57:13 -0600

    add vector calculations on post like

--- src/routes/api/posts/like/[slug]/+server.ts
+++ src/routes/api/posts/like/[slug]/+server.ts
@@ -6,19 +6,34 @@
 
 import { error, json, type RequestHandler } from "@sveltejs/kit";
 import { LikeModel } from '$lib/models/Like.model';
+import { PostModel } from "src/lib/models/Post.model";
+import { UserModel } from "src/lib/models/User.model";
 
 export const POST: RequestHandler = async({ params, request, locals }) => {
     const user = typeof locals.user === 'string' ? JSON.parse(locals.user) : locals.user;
 
     const postId = params.slug;
+    const [userDoc, post] = await Promise.all([
+        UserModel.findById(user._id),
+        PostModel.findById(postId)
+    ]);
 
-    if(!postId) {
+    if(!postId || !post || !userDoc) {
         throw error(422, 'Unprocessable Entity');
     }
 
     if(!user) {
         throw error(401, 'Unauthorized');
     }
+
+    const updatedVector = { ...(userDoc.userInterestVectors || {}) };
+
+    for (const [token, weight] of Object.entries(post.postVectors)) {
+        updatedVector[token] = (updatedVector[token] || 0) + weight;
+    }
+
+    userDoc.userInterestVectors = updatedVector;
+    await userDoc.save();
 
     try {
         const like = await LikeModel.exists({ parent_post: postId, author: user._id });
