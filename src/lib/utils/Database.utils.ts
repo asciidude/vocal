f6a8897828875849519f126ddf7b0f64ad322ad8
Brando <asciidude0@gmail.com>
2026-01-16 05:27:03 -0600

    testing something

--- src/lib/utils/Database.utils.ts
+++ src/lib/utils/Database.utils.ts
@@ -14,6 +14,8 @@ export const mongoConnection = {
 }
 
 export const connect = async () => {
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.MONGO_URI);
     if(mongoConnection.connection_code === 1) {
         console.log('A connection has already been established to MongoDB.');
         return;
