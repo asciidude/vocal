d659126e1a5a6e96ef26476704b49cb06979bc54
Brando <asciidude0@gmail.com>
2026-01-18 02:14:38 -0600

    clear settings page

--- src/routes/settings/+page.server.ts
+++ src/routes/settings/+page.server.ts
@@ -1,5 +1,9 @@
 import type { UserType } from '$lib/types/User.types';
 
+/**
+ * TODO: Link settings page to backend
+ */
+
 export const load = async ({ params, locals }) => {
     const user = locals.user as UserType | null;
     return { user }
