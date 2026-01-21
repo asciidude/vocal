3e8bd9f9730923f1a7d42ce949cf39b330ab4814
Brando <asciidude0@gmail.com>
2026-01-20 18:53:27 -0600

    remove debugging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -22,9 +22,6 @@ export const handleError = ({ error }: { error: any }) => {
 };
 
 export const handle: Handle = async ({ event, resolve }) => {
-    console.log('Request URL:', event.url.pathname, 'Method:', event.request.method);
-    console.log('Cookies:', event.cookies.get('session'));
-    
     await ensureInitialized();
 
     let user: UserType | null = null;
